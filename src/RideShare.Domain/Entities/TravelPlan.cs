d9dc0ecca10eabaea60940622401c3279858be25
handoreyn <fatihgencaslan@yahoo.com>
2023-08-19 18:52:36 +0300

    🗃️ Create TravelPlan entity

---
+++ src/RideShare.Domain/Entities/TravelPlan.cs
@@ -0,0 +1,15 @@
+using RideShare.Domain.Common;
+
+namespace RideShare.Domain.Entities;
+
+public sealed class TravelPlan : EntityBase
+{
+    public string Username { get; set; }
+    public int SeatCount { get; set; }
+    public string DepartureCity { get; set; }
+    public string DestinationCity { get; set; }
+    public string Description { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime TravelDateTime { get; set; }
+
+}
